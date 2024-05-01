cfa2a66fc6e85517c16703ec68a8f2aba565baf6
Cameron Eldridge <cameldridge@gmail.com>
2024-04-30 20:25:57 -0400

    a bit of usage of the types from the column defs

--- seedy.pl
+++ seedy.pl
@@ -18,17 +18,24 @@ main :-
 generate_migration :-
     connection(C),
     forall(table(Name, Opts), load_existing(C, Name, Opts)),
+    forall(existing(Entry), format("-- existing: ~w~n", [Entry])),
     forall(table(Name, Opts), delete_removed(Name, Opts)),
     forall(table(Name, Opts), insert_created(Name, Opts)).
 
 load_existing(C, Name, Opts) :-
-    findall(X, (member(column(X, T), Opts), \+member(generated, T)), Names),
+    cols_free(Opts, Names),
+    types_free(Opts, Types),
     Select =.. [select | Names],
     sql(C, [Select, from(Name)], data(Rows)),
-    forall((member(Row, Rows), Entry =.. [Name | Row]), assertz(existing(Entry))).
+    forall((member(Row, Rows), maplist(cast, Types, Row, Vals), Entry =.. [Name | Vals]), assertz(existing(Entry))).
+
+cast(string, V, V).
+cast(integer, S, V) :- number_chars(V, S).
+cast(float, S, V) :- number_chars(V, S).
 
 delete_removed(Name, Opts) :-
-    findall(_, (member(column(_, T), Opts), \+member(generated, T)), Vals),
+    cols_free(Opts, Names),
+    same_length(Names, Vals),
     forall(
         (Call =.. [Name | Vals], existing(Call), \+call(Call)),
         write_delete(Name, Opts, Call)
@@ -39,7 +46,8 @@ insert_created(Name, Opts) :-
         (
             findall(_, (member(column(_, T), Opts), \+member(generated, T)), Vals),
             Call =.. [Name | Vals],
-            call(Call)
+            call(Call),
+            \+existing(Call)
         ),
         write_insert(Name, Opts, Call)
     ).
@@ -50,22 +58,22 @@ atomic_join([A | As], J, S) :-
     atom_concat(A, J, S0),
     atom_concat(S0, S1, S).
 
-escape(sql(null), 'NULL') :- !.
-escape(array(C), E) :-
-    maplist(escape, C, Escaped),
+escape(_, sql(null), 'NULL') :- !.
+escape(list(T), list(C), E) :-
+    maplist(escape(T), C, Escaped),
     atomic_join(Escaped, ',', B),
     atom_concat('{', B, E0),
     atom_concat(E0, '}', E).
 
-escape(C, C) :- integer(C), !.
-escape(C, C) :- float(C), !.
-escape(C, E) :-
+escape(integer, C, C) :- integer(C), !.
+escape(float, C, C) :- float(C), !.
+escape(string, C, E) :-
     atom(C),
     atom_chars(C, Chars),
     escape_str(Chars, Escaped),
     quote(Escaped, Quoted),
     atom_chars(E, Quoted).
-escape(C, E) :-
+escape(string, C, E) :-
     escape_str(C, Escaped),
     quote(Escaped, Quoted),
     atom_chars(E, Quoted).
@@ -85,9 +93,15 @@ cols_free(Opts, Free) :-
 cols_value(Opts, Keys) :-
     findall(N, (member(column(N, T), Opts), \+member(generated, T), \+member(key, T)), Keys).
 
+types_free(Opts, Free) :-
+    findall(C, (member(column(_, T), Opts), \+member(generated, T), member(type(C), T)), Free).
+opts_free(Opts, Free) :-
+    findall(C, (member(C, Opts), C = column(_, T), \+member(generated, T)), Free).
+
 write_insert(Name, Opts, Call) :-
     Call =.. [Name | Vals],
-    maplist(escape, Vals, Escaped),
+    types_free(Opts, Types),
+    maplist(escape, Types, Vals, Escaped),
     Values =.. [values | Escaped],
     cols_free(Opts, Cols),
     cols_key(Opts, Keys),
@@ -116,9 +130,9 @@ set([Term, T2 | Terms], set((Term, Set))) :- set([T2 | Terms], set(Set)).
 
 write_delete(Name, Opts, Call) :-
     Call =.. [Name | Vals],
-    findall(C, (member(C, Opts), C = column(N, T), \+member(generated, T)), Cols),
+    opts_free(Opts, Cols),
     maplist(pair, Vals, Cols, Kvs),
-    findall(N=E, (member(V-column(N, T), Kvs), member(key, T), escape(V, E)), Cond),
+    findall(N=E, (member(V-column(N, T), Kvs), member(key, T), escape(V, _, E)), Cond),
     where(Cond, Where),
     sql_query([delete(Name), Where], Sql, []),
     format("~s;~n", [Sql]).
